8340278bc9b46733fd28dd8a42a3d799425604c7
longaodai <vochilong.work@gmail.com>
2023-09-09 22:33:59 +0700

    :tada: refactor and upgrade code

--- app/Http/Controllers/Admin/SitemapController.php
+++ app/Http/Controllers/Admin/SitemapController.php
@@ -102,9 +102,14 @@ class SitemapController extends Controller
 
     private function renewGenerateSiteMap($posts)
     {
-        $sitemapContent = '<?xml version="1.0" encoding="UTF-8"?>' . PHP_EOL;
-        $sitemapContent .= '<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' . PHP_EOL;
-        $lastmod = date('Y-m-d H:i') . ' +00:00';
+
+        $sitemapContent = '<?xml version="1.0" encoding="UTF-8"?>
+                            <urlset
+                                xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+                                xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+                                xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
+                                        http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">' . PHP_EOL;
+        $lastmod = date('Y-m-d');
 
         foreach ($this->pages as $page) {
             $url = url($page['url']);
@@ -128,7 +133,7 @@ class SitemapController extends Controller
             $sitemapContent .= '</url>' . PHP_EOL;
         }
 
-        $sitemapContent .= '</sitemapindex>';
+        $sitemapContent .= '</urlset>';
         file_put_contents(public_path('sitemaps.xml'), $sitemapContent);
 
         return true;
