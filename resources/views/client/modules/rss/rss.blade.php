8bf13f46452731e22fa0fd985804fc33ebb2449d
longaodai <vochilong.work@gmail.com>
2023-09-30 15:44:49 +0700

    :tada: add rss

--- resources/views/client/modules/rss/rss.blade.php
+++ resources/views/client/modules/rss/rss.blade.php
@@ -12,7 +12,7 @@
         @foreach($posts as $post)
             <item>
                 <title><![CDATA[{{ !empty($post->post_title) ? $post->post_title : 'Hion Coding Blog' }}]]></title>
-                <link>{{ !empty($post->post_slug) ? $post->post_slug : '/' }}</link>
+                <link>{{ !empty($post->post_slug) ? route('post-detail', ['slug' => $post->post_slug]) : route('home') }}</link>
                 <description><![CDATA[{!! !empty($post->post_description) ? $post->post_description : '' !!}]]></description>
                 <category>{{ !empty($post->category->name) ? $post->category->name : 'Hion Category' }}</category>
                 <author><![CDATA[{{ !empty($post->user->name) ? $post->user->name : 'Hion Coding Admin' }}]]></author>
