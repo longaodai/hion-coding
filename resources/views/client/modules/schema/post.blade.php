0b074f7cbae52ddbee26643ad61b6d24ed192df4
longaodai <vochilong.work@gmail.com>
2023-09-30 17:11:37 +0700

    :tada: add schema markup

--- resources/views/client/modules/schema/post.blade.php
+++ resources/views/client/modules/schema/post.blade.php
@@ -3,11 +3,11 @@
       "@context": "http://schema.org",
       "@type": "Article",
       "headline": "{{ !empty($post->post_title) ? $post->post_title : '' }}",
-      "datePublished": "{{ !empty($post->updated_at) ? formatDate($post->updated_at, 'Y-m-d') : '' }}",
+      "datePublished": "{{ !empty($post->created_at) ? formatDate($post->created_at, 'Y-m-d') : '' }}",
       "dateModified": "{{ !empty($post->updated_at) ? formatDate($post->updated_at, 'Y-m-d') : '' }}",
       "author": {
         "@type": "Person",
-        "name": ">{{ !empty($post->user->name) ? $post->user->name : 'Hion Coding Admin' }}"
+        "name": "{{ !empty($post->user->name) ? $post->user->name : 'Hion Coding Admin' }}"
       },
       "publisher": {
         "@type": "Organization",
