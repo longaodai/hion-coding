df84617cb38b8d9bbc9be04faa5c90555ec5a262
Võ Chí Long (Long Áo Dài) <87090422+longaodai@users.noreply.github.com>
2024-01-17 11:28:31 +0700

    :rocket: update for seo

--- resources/views/client/pages/vdtt.blade.php
+++ resources/views/client/pages/vdtt.blade.php
@@ -15,8 +15,28 @@
         <p><i>Nếu có bất kì câu hỏi mới hãy chia sẽ để tôi có thể update thêm vào bộ câu hỏi
                 <a rel="nofolow" target="_blank" href="https://forms.gle/fpqQYShyFhCvhuip9">Tại đây</a></i></p>
     </div>
+    <div id='list-qa'></div>
 @endsection
 
 @section('script-extend')
     <script src="{{ asset("client/vdtt/handle-search.js") }}"></script>
+    <script>
+        var listQaElement = document.getElementById('list-qa');
+        var ulElement = document.createElement('ul');
+        
+        dataQuestion.forEach(function (qa) {
+            var liElement = document.createElement('li');
+            var questionHeading = document.createElement('p');
+            questionHeading.innerHTML = `<strong><em>${qa.question}</em></strong>`;
+            var answerParagraph = document.createElement('p');
+            answerParagraph.textContent = "Trả lời: " + qa.answer;
+            liElement.appendChild(questionHeading);
+            liElement.appendChild(answerParagraph);
+            ulElement.appendChild(liElement);
+        });
+
+        if (listQaElement) {
+            listQaElement.appendChild(ulElement);
+        }
+    </script>
 @endsection
